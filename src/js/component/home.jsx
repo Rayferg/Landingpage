38fcc74f1c37ebda98409d875bdec28bbe471e5d
Rayferg <Rayferg19@gmail.com>
2022-11-20 20:01:03 +0000

    done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-
+import Bar from "./navbar.jsx";
+import Text from "./card.jsx"
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import Jumbo from "./jumbotron.jsx";
+import Foot from "./footer.jsx";
 
 //create your first component
 const Home = () => {
+	let bounce = [1,2,3,4]
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<Bar></Bar>
+			<Jumbo></Jumbo>
+			<div className="row">	
+			{bounce.map(e=><Text></Text>)}
+			</div>
+			
+			<Foot></Foot>
+			
 		</div>
 	);
 };
